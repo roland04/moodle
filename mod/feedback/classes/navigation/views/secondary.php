9fbf4c0e3f2fc39785c61e132f7c35486bc382f5
Mikel Martín <mikel@moodle.com>
2024-04-23 15:32:34 +0200

    Create a new 'Questions' secondary menu item

--- mod/feedback/classes/navigation/views/secondary.php
+++ mod/feedback/classes/navigation/views/secondary.php
@@ -34,11 +34,12 @@ class secondary extends core_secondary {
     protected function get_default_module_mapping(): array {
         $basenodes = parent::get_default_module_mapping();
         $basenodes[self::TYPE_CUSTOM] += [
-            'templatenode' => 12,
-            'mapcourse' => 13,
-            'feedbackanalysis' => 14,
-            'responses' => 15,
-            'nonrespondents' => 15.1
+            'questionnode' => 2,
+            'templatenode' => 3,
+            'responses' => 4,
+            'nonrespondents' => 4.1,
+            'feedbackanalysis' => 5,
+            'mapcourse' => 12,
         ];
 
         return $basenodes;
@@ -69,12 +70,6 @@ class secondary extends core_secondary {
             $nodesordered = $this->get_leaf_nodes($mainnode, $nodes);
             $this->add_ordered_nodes($nodesordered, $rootnode);
 
-            // Reorder the existing nodes in settings so the active node scan can pick it up.
-            $existingnode = $settingsnav->find('questionnode', self::TYPE_CUSTOM);
-            if ($existingnode) {
-                $node->add_node($existingnode);
-                $nodes[self::TYPE_CUSTOM] += ['questionnode' => 3];
-            }
             // We have finished inserting the initial structure.
             // Populate the menu with the rest of the nodes available.
             $this->load_remaining_nodes($mainnode, $nodes, $rootnode);
